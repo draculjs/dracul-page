83103ef732e9942676cab876568ccdc696188058
georgeoi <--list>
2021-01-14 17:20:39 -0300

    nuevo boton de idioma

--- components/languageSelect.vue
+++ components/languageSelect.vue
@@ -1,15 +1,37 @@
 <template>
-<div>
-    <v-icon color="black">mdi-translate</v-icon>
+<div class="ma-0 pa-0">
+    <!-- <v-icon color="black">mdi-translate</v-icon> -->
+    <v-overflow-btn
+        class="ma-0 transparent text--secondary v-btn v-btn--icon pa-0"
+        nuxt
+        height="35px"
+        aria-expanded="false"
+        :items="languages"
+        v-model="language"
+        append-icon="mdi-translate"
+        dense
+        to="/docs/installation/general_es"
+        flat
+        >
+    </v-overflow-btn>
+    
+    
+<!--     
+    
     <select v-model="language" name="selectLanguage" class="grey--text" id="language"> 
          <option value="en">EN</option>
          <option value="es">ES</option>
-     </select>
+     </select> -->
 </div>
 </template>
 <script>
 export default {
     name: "languageSelect",
+    data(){
+        return{
+            languages:["es","en"],
+        }
+    },
     computed:{
         language:{
             get(){
@@ -18,7 +40,7 @@ export default {
             set(val){
                 this.$store.commit('setLanguage', val)
             }
-        }
+        },
     }
 }
 </script>
